26c5453286ca8f6e12979c783a7f5aac17760628
serynabatov <serynabatov@yandex.ru>
2021-11-25 13:05:47 +0300

    first config

---
+++ backend/src/main/java/com/polimi/project/ehealth/EhealthApplication.java
@@ -0,0 +1,13 @@
+package com.polimi.project.ehealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhealthApplication.class, args);
+	}
+
+}
